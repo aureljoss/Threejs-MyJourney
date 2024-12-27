45ec7777ed122d631a21ae9d75b3371c59ed17af
Aurelie <josserand.n.aurelie@gmail.com>
2024-12-27 11:28:24 -0500

    Adds lesson on shader patterns

--- 28-shader-patterns/src/script.js
+++ 28-shader-patterns/src/script.js
@@ -17,45 +17,20 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-
-/**
  * Test mesh
  */
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
-console.log(geometry)
-const count = geometry.attributes.position.count
-const randoms = new Float32Array(count)
-for (let i=0; i<count; i++){
-    randoms[i]=Math.random()
-}
-geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms,1))
 
 // Material
-
-const flagTexture = textureLoader.load('/textures/flag-french.jpg')
-
-const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
     vertexShader: testVertexShader,
-    fragmentShader:testFragmentShader,
-    side: THREE.DoubleSide, 
-    uniforms:{
-        uFrequency: {value: new THREE.Vector2(10,5)}, 
-        uTime:{value:0}, 
-        uColor:{value: new THREE.Color('orange')},
-        uTexture:{value: flagTexture}
-    }
+    fragmentShader: testFragmentShader,
+    side: THREE.DoubleSide
 })
-
-console.log(material)
-
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
-mesh.scale.y=2/3
 scene.add(mesh)
 
 /**
@@ -93,10 +68,6 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**lil-gui */
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
-
 /**
  * Renderer
  */
@@ -109,17 +80,10 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-
     // Update controls
     controls.update()
-
-    //Update material
-    material.uniforms.uTime.value=elapsedTime
 
     // Render
     renderer.render(scene, camera)
