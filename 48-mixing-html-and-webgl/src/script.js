0d91b49664563ce9d1e40749f694ad6a72fd3b4a
Aurelie <josserand.n.aurelie@gmail.com>
2025-01-01 17:14:29 -0500

    Creates raycaster to test if the point is visible and moves the point according to webgl coordinates

--- 48-mixing-html-and-webgl/src/script.js
+++ 48-mixing-html-and-webgl/src/script.js
@@ -6,6 +6,7 @@ import { gsap } from 'gsap'
 /**
  * Loaders
  */
+let sceneReady=false
 const loadingBarElement = document.querySelector('.loading-bar')
 const loadingManager = new THREE.LoadingManager(
     // Loaded
@@ -21,6 +22,10 @@ const loadingManager = new THREE.LoadingManager(
             loadingBarElement.classList.add('ended')
             loadingBarElement.style.transform = ''
         }, 500)
+
+        window.setTimeout(()=>{
+            sceneReady=true
+        },3000)
     },
 
     // Progress
@@ -128,6 +133,18 @@ gltfLoader.load(
 )
 
 /**
+ * Points of interest
+ */
+
+const raycaster = new THREE.Raycaster()
+
+const points = [
+    {position: new THREE.Vector3(1.55, 0.3, -0.6),
+    element: document.querySelector('.point-0')
+    }
+]
+
+/**
  * Lights
  */
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
@@ -194,7 +211,38 @@ const tick = () =>
 {
     // Update controls
     controls.update()
+    if (sceneReady){
+         //Go through each point
+    for (const point of points){
+        const screenPosition= point.position.clone()
+        screenPosition.project(camera)
 
+        raycaster.setFromCamera(screenPosition, camera)
+        const intersects= raycaster.intersectObjects(scene.children, true)
+
+        if(intersects.length === 0){
+            point.element.classList.add('visible')
+        }
+        else {
+            const intersectionDistance= intersects[0].distance
+            const pointDistance=point.position.distanceTo(camera.position)
+
+            if(intersectionDistance<pointDistance){
+                point.element.classList.remove('visible')
+            }
+            else{
+                point.element.classList.add('visible')
+            }
+            // point.element.classList.remove('visible')
+        }
+
+        const translateX = screenPosition.x * sizes.width / 2
+        const translateY = screenPosition.y * sizes.height / 2
+        point.element.style.transform= `translateX(${translateX}px) translateY(${-translateY}px)`
+    }
+
+    }
+   
     // Render
     renderer.render(scene, camera)
 
