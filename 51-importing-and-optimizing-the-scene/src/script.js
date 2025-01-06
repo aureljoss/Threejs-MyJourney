73eb5f3f8d6343e47f091f01261450431c4acc69
Aurelie <josserand.n.aurelie@gmail.com>
2025-01-05 22:13:39 -0500

    Adds fireflies shader.

--- 51-importing-and-optimizing-the-scene/src/script.js
+++ 51-importing-and-optimizing-the-scene/src/script.js
@@ -84,19 +84,38 @@ gltfLoader.load('portal.glb',
  */
 // Geometry
 const firefliesGeometry= new THREE.BufferGeometry()
-const firefliesCount=30
+const firefliesCount=50
 const positionArray= new Float32Array(firefliesCount*3)
+const scaleArray = new Float32Array(firefliesCount)
+
 for (let i=0; i<firefliesCount; i++){
     positionArray[i*3+0]=(Math.random() -0.5) *4
     positionArray[i*3+1]=Math.random()*4
     positionArray[i*3+2]=(Math.random() - 0.5) *4
 
+    scaleArray[i]=Math.random()
+
 }
 
 firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+firefliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 1))
+
 
 //Material
-const firefliesMaterial = new THREE.PointsMaterial({size:0.1, sizeAttenuation: true})
+const firefliesMaterial = new THREE.ShaderMaterial({
+    uniforms:{
+        uTime: {value: 0},
+        uPixelRatio:{ value: Math.min(window.devicePixelRatio, 2)}, 
+        uSize: {value:200}
+    },
+    vertexShader: firefliesVertexShader, 
+    fragmentShader: firefliesFragmentShader,
+    transparent:true, 
+    blending: THREE.AdditiveBlending,
+    depthWrite:false
+})
+
+gui.add(firefliesMaterial.uniforms.uSize, 'value').min(0).max(500).step(1).name('firefliesSize')
 
 //Points
 const fireflies= new THREE.Points(firefliesGeometry, firefliesMaterial)
@@ -124,6 +143,9 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    //Update fireflies
+    firefliesMaterial.uniforms.uPixelRatio.value= Math.min(window.devicePixelRatio, 2)
 })
 
 /**
@@ -164,6 +186,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update Materials
+    firefliesMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
