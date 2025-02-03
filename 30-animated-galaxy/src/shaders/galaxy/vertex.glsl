43669280a712c22df9c3bb556ab74dbb2336c3bb
Aurelie <josserand.n.aurelie@gmail.com>
2025-02-03 16:29:08 -0500

    Adds animation and sets up vercel

--- 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -1,6 +1,8 @@
 uniform float uSize; 
+uniform float uTime; 
 
 attribute float aScale;
+attribute vec3 aRandomness;
 
 varying vec3 vColor;
  
@@ -10,6 +12,18 @@ varying vec3 vColor;
          */
 
         vec4 modelPosition = modelMatrix*vec4(position, 1.0);
+
+        // Spin
+        float angle = atan(modelPosition.x, modelPosition.z);
+        float distanceToCenter = length(modelPosition.xz);
+        float angleOffset= (1.0/distanceToCenter) *uTime*0.2;
+        angle += angleOffset;
+        modelPosition.x = cos(angle)*distanceToCenter;
+        modelPosition.z = sin(angle)*distanceToCenter;
+
+        // Randomness
+        modelPosition.xyz += aRandomness;
+
         vec4 viewPosition = viewMatrix * modelPosition;
         vec4 projectedPosition = projectionMatrix * viewPosition;
 
